a778e718977420cfaadc615c9948c0d1ebe83466
samandareo <smdeveloper04@gmail.com>
2025-02-07 20:32:26 +0500

    fix issue with extracting

--- functions.py
+++ functions.py
@@ -542,7 +542,8 @@ async def handle_start_message(message, state):
             print(phone_number, book_id)
             query = f"SELECT start_msg_id, links FROM start_messages WHERE start_msg_id = '{book_id}';"
             messages = await fetch_query(query)
-            links = json.loads(messages['links'])
+            print(messages)
+            links = json.loads(messages[0]['links'])
             with open('extras/messages.json', 'r') as file:
                 data = json.load(file)
 
