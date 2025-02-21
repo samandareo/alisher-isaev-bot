0f5bb3715cf418ad132fcb416248236954fcce63
samandareo <smdeveloper04@gmail.com>
2025-02-21 11:11:18 +0500

    remove parse mode

--- bot.py
+++ bot.py
@@ -15,7 +15,6 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from aiogram.types import Message, CallbackQuery, ReplyKeyboardRemove, FSInputFile, Poll, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.exceptions import TelegramRetryAfter
 from aiogram.utils.markdown import link
-from aiogram.utils import markdown
 from database import execute_query, fetch_query, init_db
 import functions as fns
 from State.userState import UserState, AdminState, AdminStateOne, UserMessagesToAdmin, CreatePoll, PollResults, ChangeBooks, WelcomePoll
@@ -157,9 +156,9 @@ async def rasilka(users, message):
             break
         try:
             if message.text:
-                await bot.send_message(user['user_id'],message.text.replace("$name", user['name']), disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN_V2)
+                await bot.send_message(user['user_id'],message.text.replace("$name", user['name']), disable_web_page_preview=True)
             elif message.caption:
-                await bot.copy_message(user['user_id'],message.chat.id,message.message_id, caption=message.caption.replace("$name", user['name']), protect_content=True, parse_mode=ParseMode.MARKDOWN_V2)
+                await bot.copy_message(user['user_id'],message.chat.id,message.message_id, caption=message.caption.replace("$name", user['name']), protect_content=True)
             elif not message.text and not message.caption:
                 await bot.copy_message(user['user_id'],message.chat.id,message.message_id, protect_content=True)
             print(f"Message sent to {user['name']} ({user['user_id']})")
@@ -168,9 +167,9 @@ async def rasilka(users, message):
             print(f'Rate limit exceeded. Sleeping for {e.timeout} seconds.')
             await asyncio.sleep(e.timeout)
             if message.text:
-                await bot.send_message(user['user_id'],message.text.replace("$name", user['name']), disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN_V2)
+                await bot.send_message(user['user_id'],message.text.replace("$name", user['name']), disable_web_page_preview=True)
             elif message.caption:
-                await bot.copy_message(user['user_id'],message.chat.id,message.message_id, caption=message.caption.replace("$name", user['name']), protect_content=True, parse_mode=ParseMode.MARKDOWN_V2)
+                await bot.copy_message(user['user_id'],message.chat.id,message.message_id, caption=message.caption.replace("$name", user['name']), protect_content=True)
             elif not message.text and not message.caption:
                 await bot.copy_message(user['user_id'],message.chat.id,message.message_id, protect_content=True)
             print(f"Message sent to {user['name']} ({user['user_id']})")
@@ -239,9 +238,9 @@ async def send_to_one(message: Message, state: FSMContext) -> None:
     user = await fetch_query(f"SELECT name FROM users WHERE user_id = '{user_id}';")
     try:
         if message.text:
-            await bot.send_message(chat_id=user_id,text=message_text.replace("$name", user[0]['name']), disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN_V2)
+            await bot.send_message(chat_id=user_id,text=message_text.replace("$name", user[0]['name']), disable_web_page_preview=True)
         elif message.caption:
-            await bot.copy_message(user_id,message.chat.id,message.message_id, caption=message.caption.replace("$name", user[0]['name']), parse_mode=ParseMode.MARKDOWN_V2)
+            await bot.copy_message(user_id,message.chat.id,message.message_id, caption=message.caption.replace("$name", user[0]['name']))
         elif not message.text and not message.caption:
             await bot.copy_message(user_id,message.chat.id,message.message_id)
         await message.answer("Xabar jo'natildi!")
